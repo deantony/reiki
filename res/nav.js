cde59416d28687d0c35dab9db1fecabbe28bb679
Tony <52037922+deantony@users.noreply.github.com>
2020-08-31 02:23:23 -0600

    Update nav.js

--- res/nav.js
+++ res/nav.js
@@ -6,6 +6,8 @@ const nav_home = document.getElementById("nav_home");
 const nav_about = document.getElementById("nav_about");
 const nav_book = document.getElementById("nav_book");
 
+const scaler = 1.15 * -1;
+
 let url = window.location.pathname;
 
 if (url === home){
@@ -15,9 +17,9 @@ if (url === home){
     }
     nav_home.onmouseenter = function(){
         nav_home.style.cursor = "default";
+        nav_home.style.scale = scaler;
     }
 }else if (url === about){
-    nav_about.style.backgroundColor = "rgb(25, 15, 25)";
     nav_about.style.boxShadow = "inset 0 4px 12px 0 rgba(0, 0, 0, .5)";
     nav_about.onclick = function(){
         return false;
@@ -26,7 +28,6 @@ if (url === home){
         nav_about.style.cursor = "default";
     }
 }else if (url === book){
-    nav_book.style.backgroundColor = "rgb(25, 15, 25)";
     nav_book.style.boxShadow = "inset 0 4px 12px 0 rgba(0, 0, 0, .5)";
     nav_book.onclick = function(){
         return false;
